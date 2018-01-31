cef2d8b0dbdf2ff0cdf6c621b5922a0632ae95b5
tomassimkus <to>
2018-01-31 10:41:31 +0200

    Pabaigtas skaiciuotuvas

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Iveskite sveikaji skaiciu");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
         LyginisNelyginis objektas = new LyginisNelyginis();
+        objektas.set_a(a);
+        System.out.println("Skaicius a: " + objektas.get_a());
+        objektas.skaiciuok();
     }
 }
--- src/uzdaviniai/LyginisNelyginis.java
+++ src/uzdaviniai/LyginisNelyginis.java
@@ -4,11 +4,16 @@ import java.util.Scanner;
 
 public class LyginisNelyginis {
 
-    public static void arSkaiciusLyginis () {
-        System.out.println("Iveskite sveikaji skaiciu");
-        Scanner sc = new Scanner(System.in);
-        int skaicius = sc.nextInt();
-        if (skaicius % 2 == 0) {
+    private int _a;
+
+    public int get_a() {
+        return _a;
+    }
+
+    public void set_a(int _a) { this._a = _a;}
+
+    public void skaiciuok() {
+        if (_a % 2 == 0) {
             System.out.println("Skaicius yra lyginis");
         } else {
             System.out.println("Skaicius nera lyginis");
