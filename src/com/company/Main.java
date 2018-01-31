677226b344922ef1fcdf6af298dd887d6029229b
tomassimkus <to>
2018-01-31 10:20:24 +0200

    padaryta uzduotis

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Iveskite sveikaji skaiciu");
+        Scanner sc = new Scanner(System.in);
+        int skaicius = sc.nextInt();
+        if (skaicius % 2 == 0) {
+            System.out.println("Skaicius yra lyginis");
+        } else {
+            System.out.println("Skaicius nera lyginis");
+        }
     }
 }
